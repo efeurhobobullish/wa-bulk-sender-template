10cdcb0086e2476228053fdec08562f30d8d60da
Empire Tech Labs <151479661+efeurhobobullish@users.noreply.github.com>
2026-05-22 18:44:03 +0100

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GithubAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "your apikey",
-  authDomain: "your authdomain",
-  projectId: "your projectid",
-  storageBucket: "your storagebucket",
-  messagingSenderId: "your messagingSenderId",
-  appId: "your appid",
+  apiKey: "AIzaSyCQ34F5SsjS7e2_yoJdDPwjrP66kJ0HH-g",
+  authDomain: "wa-bulk-sender-template.firebaseapp.com",
+  projectId: "wa-bulk-sender-template",
+  storageBucket: "wa-bulk-sender-template.firebasestorage.app",
+  messagingSenderId: "249305684771",
+  appId: "1:249305684771:web:be30309fdb910fe0a44a96",
+  measurementId: "G-1V9PQ9Y0YK"
 };
 
 const app = initializeApp(firebaseConfig);
